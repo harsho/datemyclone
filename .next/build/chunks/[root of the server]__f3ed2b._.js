b9a9863def2de9ee15a35f871a8719da059a9407
Harsha Ogoti <harshao@Harshas-MacBook-Pro-2.local>
2024-12-15 20:40:30 -0500

    deploy commit

--- .next/build/chunks/[root of the server]__f3ed2b._.js
+++ .next/build/chunks/[root of the server]__f3ed2b._.js
@@ -48,8 +48,11 @@ __turbopack_esm__({
     "default": (()=>transform),
     "init": (()=>init)
 });
-// @ts-ignore
-var __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$postcss$2f$lib$2f$postcss$2e$mjs__$5b$postcss$5d$__$28$ecmascript$29$__ = __turbopack_import__("[project]/node_modules/postcss/lib/postcss.mjs [postcss] (ecmascript)");
+(()=>{
+    const e = new Error("Cannot find module '@vercel/turbopack/postcss'");
+    e.code = 'MODULE_NOT_FOUND';
+    throw e;
+})();
 // @ts-ignore
 var __TURBOPACK__imported__module__$5b$project$5d2f$postcss$2e$config$2e$mjs__$5b$postcss$5d$__$28$ecmascript$29$__ = __turbopack_import__("[project]/postcss.config.mjs [postcss] (ecmascript)");
 var __TURBOPACK__imported__module__$5b$externals$5d2f$__$5b$external$5d$__$28$path$2c$__cjs$29$__ = __turbopack_import__("[externals]/ [external] (path, cjs)");
@@ -108,7 +111,7 @@ const init = async (ipc)=>{
         }
         return plugin;
     });
-    processor = (0, __TURBOPACK__imported__module__$5b$project$5d2f$node_modules$2f$postcss$2f$lib$2f$postcss$2e$mjs__$5b$postcss$5d$__$28$ecmascript$29$__["default"])(loadedPlugins);
+    processor = postcss(loadedPlugins);
 };
 async function transform(ipc, cssContent, name) {
     const { css, map, messages } = await processor.process(cssContent, {
